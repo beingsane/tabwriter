290c4397dbaab4b8c86347bae6892ae6a4ecd0e5
Raphael <raphaeljjj@gmail.com>
2017-06-22 18:04:41 -0300

    set files for deployment on heroku

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.set('port', (process.env.PORT || 3000));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html");
@@ -15,6 +16,6 @@ app.get("*", function(req, res){
   res.status(404).send("Not found!")
 });
 
-app.listen(3000, function(){
+app.listen(app.get("port"), function(){
   console.log("TabWriter initialized on port 3000");
 });
