806aec73b96052f3d97f49684bffd045f41fa84b
raphael-jorge <raphaeljjj@gmail.com>
2018-01-24 19:11:56 -0200

    feat: Add context menu in the about page
      - Set <a> tag for internal links

--- public/js/src/about.js
+++ public/js/src/about.js
@@ -1,5 +1,3 @@
-const animations = require('./animations.js');
-
 const about = {
   init: function() {
     this.view.init();
@@ -8,13 +6,66 @@ const about = {
   view: {
     init: function() {
       this.TOOLTIP_FADE_DELAY = 350;
-
+      this.window = $(window);
+      this.document = $(document);
       this.exampleSel = '#example';
       this.example = $(this.exampleSel);
       this.instructionsBtn = $('#instructions-go');
       this.instructionsArea = $('#instructions');
+      this.sectionsNav = $('#sections-nav');
 
+      this.sections = [];
+      $('a.anchor-link').each(function () {
+        const name = $(this).attr('name');
+        const section = {
+          name: name,
+          top: $(this).offset().top
+        };
+        about.view.sections.push(section);
+      });
 
+      this.setEventListeners();
+      this.render();
+    },
+
+    render: function() {
+      // Reset sections nav
+      this.sectionsNav.find('li').removeClass('active');
+      // Get current section on screen
+      const windowTop = this.window.scrollTop();
+      let sectionIdx;
+      if (this.sections.length > 1) {
+        const secondItenTop = this.sections[1].top;
+        const lastItenTop = this.sections.slice(-1)[0].top;
+        if (windowTop < secondItenTop) {
+          sectionIdx = 0;
+        } else if (windowTop >= lastItenTop) {
+          sectionIdx = this.sections.length - 1;
+        } else {
+          for (let i = 1, n = this.sections.length; i < n; i++) {
+            if (windowTop >= this.sections[i].top && windowTop < this.sections[i + 1].top) {
+              sectionIdx = i;
+              break;
+            }
+          }
+        }
+      } else {
+        sectionIdx = 0;
+      }
+      // Active current section
+      const sectionNav = this.sectionsNav.find('a[href="#' +
+                                    this.sections[sectionIdx].name + '"]');
+      const secNavFirstParent = sectionNav.parents().eq(0);
+      const secNavThirdParent = sectionNav.parents().eq(2);
+      if (secNavFirstParent.prop('tagName') === 'LI') {
+        secNavFirstParent.addClass('active');
+      }
+      if (secNavThirdParent.prop('tagName') === 'LI') {
+        secNavThirdParent.addClass('active');
+      }
+    },
+
+    setEventListeners: function() {
       if (Clipboard.isSupported()) {
         let clipboard = new Clipboard(this.exampleSel);
 
@@ -32,13 +83,12 @@ const about = {
         });
       }
 
-      this.instructionsBtn.on('click', (event) => {
+      this.instructionsBtn.on('click', () => {
         this.instructionsArea.addClass('highlight');
-        let rect = this.instructionsArea.get(0).getBoundingClientRect();
-        let navbarHeight = parseInt($('nav').css('height'));
-        let offset = 10;
-        animations.scrollBy(rect.top - (navbarHeight + offset), 400);
-        event.preventDefault();
+      });
+
+      $(window).on('scroll', () => {
+        this.render();
       });
     }
 
