3dc693cc31bddebf69e92bc73ba03292c483b689
raphael.jorge <raphaeljjj@gmail.com>
2020-07-26 21:10:02 -0300

    feat: update client dependencies

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const proxy = require('http-proxy-middleware');
+const {
+  createProxyMiddleware
+} = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use(
     '/api',
-    proxy({
+    createProxyMiddleware({
       target: process.env.REACT_APP_BACKEND_SERVER_URL,
       changeOrigin: true,
     })
